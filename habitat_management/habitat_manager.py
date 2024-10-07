ea598a1176b35bf34320c3a136a1bdbd19c771c2
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 19:45:12 -0400

    move create_habitat habitat manager file

--- habitat_management/habitat_manager.py
+++ habitat_management/habitat_manager.py
@@ -3,4 +3,8 @@ from typing import Optional, List
 class HabitatManager:
 
     def assign_animals_to_habitat(habitat_id: int, animals: List[Animal]) -> None:
-    pass
+        pass
+
+    def create_habitat(habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
+        pass
+
