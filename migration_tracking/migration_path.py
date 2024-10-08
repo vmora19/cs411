8a3e1f1ae2f3246bd00dc2e587989f97a8a9a63c
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:13:45 -0400

    Update migration_path.py

--- migration_tracking/migration_path.py
+++ migration_tracking/migration_path.py
@@ -4,3 +4,6 @@ class MigrationPath:
 
     def update_migration_path_details(path_id: int, **kwargs) -> None:
         pass
+
+    def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
+        pass
