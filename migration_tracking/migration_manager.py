d3d86780677b2c2cd438d62c14cae0b791dda36e
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:02:34 -0400

    adding final methods with "get migration"

--- migration_tracking/migration_manager.py
+++ migration_tracking/migration_manager.py
@@ -33,3 +33,12 @@ class MigrationManager:
     def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
         pass
 
+    def get_migrations_by_start_date(start_date: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_status(status: str) -> list[Migration]:
+        pass
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
+
