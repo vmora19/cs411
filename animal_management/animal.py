0d1e958e233277fb22a4dbbec0a1410520ce3002
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:50:25 -0400

    Update animal.py

--- animal_management/animal.py
+++ animal_management/animal.py
@@ -2,22 +2,8 @@ from typing import Any, Optional
 
 class Animal:
 
-    def __init__(self,
-                habitat_id: int,
-                geographic_area: str,
-                size: int,
-                environment_type: str,
-                animals: Optional[List[int]] = None) -> None:
-        self.habitat_id = habitat_id
-        self.geographic_area = geographic_area
-        self.size = size
-        self.environment_type = environment_type
-        # this is Pythonic for
-        # if animals is not None:
-        #   self.animals = animals
-        # else:
-        #   self.animals = []
-        self.animals = animals or []
+    def __init__(self, animal_id: int):      
+        self.animal_id = animal_id
     
     def update_animal_details(animal_id: int, **kwargs: Any) -> None:
         pass
