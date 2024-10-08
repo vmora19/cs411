d87dc804d470c8a3836a985483efe5516fd4a62e
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:01:42 -0400

    commenting out functions we've moved

--- formal_specification.py
+++ formal_specification.py
@@ -66,29 +66,29 @@ def get_animals_in_habitat(habitat_id: int) -> List[Animal]:
 #def get_habitats_by_type(environment_type: str) -> List[Habitat]:
     #pass
 
-def get_migration_by_id(migration_id: int) -> Migration:
-    pass
+#def get_migration_by_id(migration_id: int) -> Migration:
+    #pass
 
-def get_migration_details(migration_id: int) -> dict[str, Any]:
-    pass
+#def get_migration_details(migration_id: int) -> dict[str, Any]:
+    #pass
 
-def get_migration_path_by_id(path_id: int) -> MigrationPath:
-    pass
+#def get_migration_path_by_id(path_id: int) -> MigrationPath:
+    #pass
 
-def get_migration_paths() -> list[MigrationPath]:
-    pass
+#def get_migration_paths() -> list[MigrationPath]:
+    #pass
 
-def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
-    pass
+#def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
+    #pass
 
-def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
-    pass
+#def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
+    #pass
 
-def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
-    pass
+#def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
+    #pass
 
-def get_migrations() -> list[Migration]:
-    pass
+#def get_migrations() -> list[Migration]:
+    #pass
 
 def get_migrations_by_current_location(current_location: str) -> list[Migration]:
     pass
