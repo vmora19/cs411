b2ba053505b2553e259a77d77f85e3d1d2f21b29
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:16:43 -0400

    Update migration_path.py

--- migration_tracking/migration_path.py
+++ migration_tracking/migration_path.py
@@ -7,3 +7,18 @@ class MigrationPath:
 
     def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
         pass
+
+    def get_migration_path_by_id(path_id: int) -> MigrationPath:
+        pass
+
+    def get_migration_paths() -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
+        pass
