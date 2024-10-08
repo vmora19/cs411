c5b22988c6220b0d6b47474fe5081d53fd1a9654
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:52:19 -0400

    Update migration.py

--- migration_tracking/migration.py
+++ migration_tracking/migration.py
@@ -2,6 +2,9 @@ from typing import Any
 
 class Migration:
 
+    def __init__(self, migration_id: int):      
+        self.migration_id = migration_id
+
     def update_migration_details(migration_id: int, **kwargs: Any) -> None:
         pass
 
