1cc9afbc99e378d40addc5caeca918952b009150
vmora19 <157311520+vmora19@users.noreply.github.com>
2024-10-07 20:16:30 -0400

    Update migration_manager.py

--- migration_tracking/migration_manager.py
+++ migration_tracking/migration_manager.py
@@ -9,21 +9,6 @@ class MigrationManager:
     def get_migration_details(migration_id: int) -> dict[str, Any]:
         pass
 
-    def get_migration_path_by_id(path_id: int) -> MigrationPath:
-        pass
-
-    def get_migration_paths() -> list[MigrationPath]:
-        pass
-
-    def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
-        pass
-
-    def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
-        pass
-
-    def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
-        pass
-
     def get_migrations_by_current_location(current_location: str) -> list[Migration]:
         pass
 
